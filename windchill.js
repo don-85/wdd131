9de5b9f3a5a5bf70b763b1ae4eabc0100906436d
don-85 <muntutitus17@gmail.com>
2024-10-02 21:16:38 +0300

    created the place.html, css and js

---
+++ windchill.js
@@ -0,0 +1,5 @@
+function calculateWindChill(temperature, windSpeed, isCelsius = true) {
+    return isCelsius 
+        ? (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16))
+        : (35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16));
+}
